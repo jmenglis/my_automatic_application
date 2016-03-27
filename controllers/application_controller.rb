d68511ea6c5a6c064481ac72f4465157d6c375ab
Josh English <josh@joshenglish.com>
2016-03-27 14:20:26 -0500

    updating to send direct message via api

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,11 +1,20 @@
 class ApplicationController < Sinatra::Base
 
+  Dotenv.load
   # Set Views and Public Dir to proper directory
   set :views, File.expand_path('../../views', __FILE__)
   set :public_dir, File.expand_path('../../public', __FILE__)
 
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key = ENV["CONSUMER_KEY"]
+    config.consumer_secret = ENV["CONSUMER_SECRET"]
+    config.access_token = ENV["ACCESS_TOKEN"]
+    config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
+  end
 
-  get '/' do
-
+  get '/dm' do
+    client.create_direct_message('joshenglish','Thank you for following @redothecube.  Please follow my journey through code bootcamp and launching my business at redothecube.com/code.  You can sign up to receive the email updates.  I promise not to spam you.')
   end
 end
+
+get '/users' do
